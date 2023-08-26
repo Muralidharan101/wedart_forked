8ddf44dfcc525007a8f9bc114d2c7e653a05ed4b
NAINARAB <karthickab77@gmail.com>
2023-08-26 11:56:23 +0530

    login

--- login.php
+++ login.php
@@ -2,39 +2,50 @@
 <html lang="en">
   
 <!-- Mirrored from admin.pixelstrap.com/koho/template/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 28 Jul 2023 10:01:29 GMT -->
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Koho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Koho admin template, dashboard template, flat admin template, responsive admin template, web app">
-    <meta name="author" content="pixelstrap">
-    <link rel="icon" href="../assets/images/favicon/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="../assets/images/favicon/favicon.png" type="image/x-icon">
-    <title>Koho - Premium Admin Template</title>
-    <!-- Google font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/font-awesome.css">
-    <!-- ico-font-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/icofont.css">
-    <!-- Themify icon-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/themify.css">
-    <!-- Flag icon-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/flag-icon.css">
-    <!-- Feather icon-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/feather-icon.css">
-    <!-- Bootstrap css-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/vendors/bootstrap.css">
-    <!-- App css-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-    <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen">
-    <!-- Responsive css-->
-    <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
-  </head>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description"
+    content="Koho admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+  <meta name="keywords"
+    content="admin template, Koho admin template, dashboard template, flat admin template, responsive admin template, web app">
+  <meta name="author" content="pixelstrap">
+  <link rel="icon" href="../assets/images/favicon/favicon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="../assets/images/favicon/favicon.png" type="image/x-icon">
+  <title>Wedart</title>
+  <!-- Google font-->
+  <link rel="preconnect" href="https://fonts.googleapis.com/">
+  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/font-awesome.css">
+  <!-- ico-font-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/icofont.css">
+  <!-- Themify icon-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/themify.css">
+  <!-- Flag icon-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/flag-icon.css">
+  <!-- Feather icon-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/feather-icon.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/scrollbar.css">
+  <!-- Bootstrap css-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/vendors/bootstrap.css">
+  <!-- App css-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+  <link id="color" rel="stylesheet" href="../assets/css/color-1.css" media="screen">
+  <!-- Responsive css-->
+  <link rel="stylesheet" type="text/css" href="../assets/css/responsive.css">
+
+  <link rel="stylesheet" type="text/css" href="/wedartfiles/customstyle.css">
+</head>
   <body>
     <!-- Loader starts-->
     <div class="loader-wrapper">
@@ -47,44 +58,36 @@
         <div class="col-12 p-0">    
           <div class="login-card">
             <div>
-              <div><a class="logo" href="index.html"><img class="img-fluid for-light" src="../assets/images/logo/logo.png" alt="logo image"></a></div>
               
               <div class="login-main"> 
+              <div class="cntr">
+                <div class="logodiv">
+                </div>
+              </div>
                 <form class="theme-form">
                   <h2 class="text-center">Sign in to account</h2>
-                  <p class="text-center">Enter your email & password to login</p>
+                  <br>
                   <div class="form-group">
                     <label class="col-form-label">Phone Number</label>
-                    <input class="form-control" type="email" required="" placeholder="9876543210">
+                    <input class="form-control" type="email" required="" style="border: 1px solid #e0dddd">
                   </div>
                   <div class="form-group">
                     <label class="col-form-label">Password</label>
                     <div class="form-input position-relative">
-                      <input class="form-control" type="password" name="login[password]" required="" placeholder="*********">
+                      <input class="form-control" type="password" name="login[password]" required="" style="border: 1px solid #e0dddd">
                       <div class="show-hide"><span class="show">                         </span></div>
                     </div>
                   </div>
-                  <div class="form-group mb-0">
-                    <div class="checkbox p-0">
-                      <input id="remember" type="checkbox" class="checkbox_animated">
+                  <div class="mb-4">
+                    <div style="margin-top: 2em;">
+                      <input id="remember" type="checkbox"  checked="">
                       <label class="text-muted" for="remember">Remember Me</label>
-                    </div><a class="link" href="forget-password.html">Forgot password?</a>
+                    </div>
                     <div class="text-end mt-3">
-                      <button class="btn btn-primary btn-block w-100" type="submit">Sign in                 </button>
+                      <button class="btn btn-primary btn-block w-100" type="submit">Sign in</button>
                     </div>
                   </div>
-                  <div class="login-social-title">
-                    <h3>Or Sign in with                 </h3>
-                  </div>
-                  <div class="form-group">
-                    <ul class="login-social">
-                      <li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-                      <li><a href="https://www.linkedin.com/" target="_blank"><i class="fa fa-linkedin"> </i></a></li>
-                      <li><a href="https://www.twitter.com/" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                      <li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-                    </ul>
-                  </div>
-                  <p class="mt-4 mb-0 text-center">Don't have account?<a class="ms-2" href="sign-up.html">Create Account</a></p>
+                  
                 </form>
               </div>
             </div>
@@ -92,16 +95,41 @@
         </div>
       </div>
       <!-- latest jquery-->
+      <style>
+        .cntr{
+          display: flex;
+          justify-content: center;
+          margin-bottom: 1em;
+        }
+        .logodiv{
+          background-image: url('wedart.jpeg');
+          background-size: cover;
+          background-repeat: no-repeat;
+          background-position: fixed;
+          height: 60px;
+          width: 150px;
+        }
+      </style>
       <script src="../assets/js/jquery-3.6.0.min.js"></script>
       <script src="../assets/js/bootstrap/popper.min.js"></script>
       <script src="../assets/js/bootstrap/bootstrap.bundle.min.js"></script>
       <!-- feather icon js-->
       <script src="../assets/js/icons/feather-icon/feather.min.js"></script>
       <script src="../assets/js/icons/feather-icon/feather-icon.js"></script>
+      <script src="../assets/js/scrollbar/simplebar.js"></script>
+      <script src="../assets/js/scrollbar/custom.js"></script>
       <!-- Sidebar jquery-->
       <script src="../assets/js/config.js"></script>
+      <script src="../assets/js/sidebar-menu.js"></script>
+      <script src="../assets/js/tooltip-init.js"></script>
       <!-- Theme js-->
       <script src="../assets/js/script.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
+        integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     </div>
   </body>
 
