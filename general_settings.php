5545f6c06b34a6d22ba56349b098801091048ebe
NAINARAB <karthickab77@gmail.com>
2023-08-28 11:26:01 +0530

    header and sidenav

--- general_settings.php
+++ general_settings.php
@@ -50,47 +50,7 @@
   <!-- page-wrapper Start-->
   <div class="page-wrapper compact-wrapper" id="pageWrapper">
     <!-- Page Header Start-->
-    <div class="page-header">
-      <div class="header-wrapper row m-0">
-        <form class="form-inline search-full col" action="#" method="get">
-          <div class="form-group w-100">
-            <div class="Typeahead Typeahead--twitterUsers">
-              <div class="u-posRelative">
-                <input class="demo-input Typeahead-input form-control-plaintext w-100" type="text" placeholder="Search Koho .." name="q" title="" autofocus>
-                <div class="spinner-border Typeahead-spinner" role="status"><span class="sr-only">Loading...</span></div><i class="close-search" data-feather="x"></i>
-              </div>
-              <div class="Typeahead-menu"></div>
-            </div>
-          </div>
-        </form>
-        <div class="header-logo-wrapper col-auto p-0">
-          <div class="logo-wrapper"><a href="index.html">
-          <!-- <img height="40" width="140" src="wedart.jpeg" alt="er"> -->
-          </a></div>
-          <div class="toggle-sidebar"><i class="status_toggle middle sidebar-toggle" data-feather="align-center"></i></div>
-        </div>
-        <div class="left-header col horizontal-wrapper ps-0">
-          <div class="input-group" style="background-color: transparent;">
-            <img class="img-fluid for-light" height="40" width="110" src="wedart.jpeg" alt="">
-          </div>
-        </div>
-        <div class="nav-right col-6 pull-right right-header p-0">
-          <ul class="nav-menus">
-                        
-            <li class="profile-nav onhover-dropdown p-0 me-0">
-              <div class="d-flex profile-media"><img class="b-r-50" src="../assets/images/dashboard/profile.png" alt="">
-                <div class="flex-grow-1"><span>Helen Walter</span>
-                  <p class="mb-0 font-roboto">Admin <i class="middle fa fa-angle-down"></i></p>
-                </div>
-              </div>
-              <ul class="profile-dropdown onhover-show-div">
-                <li><a href="login.php"><i data-feather="log-in"> </i><span>Log Out</span></a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <?php include 'header.php'; ?>
     <!-- Page Header Ends                              -->
     <!-- Page Body Start-->
     <div class="page-body-wrapper">
